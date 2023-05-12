2185b212809fce05d102fa545a5ae7176a5d6c22
Douglas81 <douglas81silveira@gmail.com>
2023-05-12 11:14:03 -0300

    implementados componentes card, menu-bar e menu-logo

---
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector: 'app-menu-bar',
+	templateUrl: './menu-bar.component.html',
+	styleUrls: ['./menu-bar.component.css'],
+})
+export class MenuBarComponent {
+	@Input()
+	logoSrc: string = '';
+}
---
+++ src/app/components/menu-bar/menu-logo/menu-logo.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector: 'app-menu-logo',
+	templateUrl: './menu-logo.component.html',
+	styleUrls: ['./menu-logo.component.css'],
+})
+export class MenuLogoComponent {
+	@Input()
+	src: string = '';
+}
